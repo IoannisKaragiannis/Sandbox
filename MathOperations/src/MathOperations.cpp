3a58cec1842b7a81b7ebba3978d10d35cc0dd173
IoannisKaragiannis <ioanniskaragiannis1987@gmail.com>
2018-09-09 01:34:16 +0200

    Add more functions

--- MathOperations/src/MathOperations.cpp
+++ MathOperations/src/MathOperations.cpp
@@ -21,6 +21,30 @@ float MyNorm(std::vector<float> v)
 	return sqrt(result);
 }
 
+float MyMax(float x, float y)
+{
+	if(x >= y)
+	{
+		return x;
+	}
+	else
+	{
+		return y;
+	}
+}
+
+float MyMin(float x, float y)
+{
+	if(x < y)
+	{
+		return x;
+	}
+	else
+	{
+		return y;
+	}
+}
+
 int main()
 {
 	std::cout << "########## PROGRAM STARTED ############" << std::endl;
@@ -32,6 +56,8 @@ int main()
 	v.push_back(5.9);
 
 	std::cout << "norm = " << MyNorm(v) << std::endl;
+	std::cout << "max(" << v[0] << "," << v[1] << ") = " << MyMax(v[0],v[1]) << std::endl;
+	std::cout << "min(" << v[0] << "," << v[1] << ") = " << MyMin(v[0],v[1]) << std::endl;
 
 	std::cout << "########## PROGRAM FINISHED ############" << std::endl;
 	return 0;
